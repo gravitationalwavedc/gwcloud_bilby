28e2290392306279282610b5b4a5d59e6748ec6f
Lewis Lakerink <llakerink@swin.edu.au>
2020-05-06 06:47:07 +1000

    Add business logic for handling GWOSC and simulated jobs. General refactoring of submit code and settings

--- src/bundle/core/misc.py
+++ src/bundle/core/misc.py
@@ -1,2 +1,7 @@
+import os
+
+from settings import job_directory
+
+
 def working_directory(details, job_data):
-    return "/fred/oz988/gwcloud/jobs/" + str(details['job_id'])
+    return os.path.join(job_directory + str(details['job_id']))
