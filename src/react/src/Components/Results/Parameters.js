b0114a1cd80d8741de8878b6d8b1e0b63301e350
Lewis Lakerink <llakerink@swin.edu.au>
2021-05-31 14:10:28 +1000

    Fix null parameter crash
    Summary: There is a case for jobs with no parameter information kept in the database which causes the web page to crash.
    
    Test Plan: Works as expected in production for hand uploaded jobs and doesn't crash.
    
    Reviewers: TomReichardt, jesmigel
    
    Reviewed By: TomReichardt
    
    Differential Revision: https://phab.adacs.org.au/D520

--- src/react/src/Components/Results/Parameters.js
+++ src/react/src/Components/Results/Parameters.js
@@ -5,13 +5,16 @@ import ReviewJob from '../Forms/ReviewJob';
 const Parameters = (props) => {
     const jobData = props.jobData ? props.jobData : props.parameters;
     const values = Object.keys(jobData).reduce((result, key) => {
+        if (!jobData[key])
+            return {};
+
         Object.keys(jobData[key]).map((item) => {
             result[item] = jobData[key][item];
         });
         return result;
     }, {});
-  
-    return <ReviewJob values={values} />;
+
+    return <ReviewJob values={values}/>;
 };
 
 export default createFragmentContainer(Parameters, {
