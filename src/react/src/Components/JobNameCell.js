62af379bfbfcb37fb90c4f068292c590af708b56
Asher <jasherleslie@gmail.com>
2022-03-17 05:03:17 +0000

    Fix description error
    Currently the new job table gets out of alignment when there are very long descriptions (>250 characters). This mr adds an explicit column size that stops this happening.
    
    It also adds migrations to the start script for local development.
    
    Approved-by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwcloud_bilby/-/merge_requests/52

--- src/react/src/Components/JobNameCell.js
+++ src/react/src/Components/JobNameCell.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 
-const JobNameCell = ({author, jobName, description}) => <Col>
-    {author ? <span>{author} | </span> : null}<span style={{fontWeight: 700}}>{jobName}</span><br/>{description}
+const JobNameCell = ({author, jobName, description}) => <Col md={6}>
+    {author ? <span>{author} | </span> : null}
+    <span style={{fontWeight: 700}}>{jobName}</span><br/>
+    {description}
 </Col>;
 
 export default JobNameCell;
