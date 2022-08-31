8096bd97074b1646f8ba44b3963158668252fa3d
Lewis Lakerink <llakerink@swin.edu.au>
2022-08-31 15:21:25 +1000

    fix jsx-quotes

--- src/react/src/Components/Forms/DataForm.js
+++ src/react/src/Components/Forms/DataForm.js
@@ -29,13 +29,13 @@ const DataForm = ({formik, handlePageChange, data}) => {
     return <React.Fragment>
         <Row>
             <Col>
-                <FormCard title="Data">
+                <FormCard title='Data'>
                     <Row>
                         <Col>
                             <RadioGroup
-                                title="Types of data"
+                                title='Types of data'
                                 formik={formik}
-                                name="dataChoice"
+                                name='dataChoice'
                                 options={[
                                     {label: 'Real', value: 'real'},
                                     {label: 'Simulated', value: 'simulated'}
@@ -50,9 +50,9 @@ const DataForm = ({formik, handlePageChange, data}) => {
                             />
                             <Input
                                 formik={formik}
-                                title="Trigger time (GPS)"
-                                name="triggerTime"
-                                type="number"
+                                title='Trigger time (GPS)'
+                                name='triggerTime'
+                                type='number'
                                 onChange={e => {
                                     setEventId(null);
                                     formik.getFieldProps('triggerTime').onChange(e);
@@ -62,37 +62,37 @@ const DataForm = ({formik, handlePageChange, data}) => {
                     </Row>
                     <Row>
                         <Col>
-                            <Form.Group controlId="samplingFrequency">
+                            <Form.Group controlId='samplingFrequency'>
                                 <Form.Label>Sampling frequency</Form.Label>
                                 <Form.Control
-                                    name="samplingFrequency"
-                                    as="select"
+                                    name='samplingFrequency'
+                                    as='select'
                                     custom
                                     {...formik.getFieldProps('samplingFrequency')}>
-                                    <option value="512">512 hz</option>
-                                    <option value="1024">1024 hz</option>
-                                    <option value="2048">2048 hz</option>
-                                    <option value="4096">4096 hz</option>
-                                    <option value="8192">8192 hz</option>
-                                    <option value="16384">16384 hz</option>
+                                    <option value='512'>512 hz</option>
+                                    <option value='1024'>1024 hz</option>
+                                    <option value='2048'>2048 hz</option>
+                                    <option value='4096'>4096 hz</option>
+                                    <option value='8192'>8192 hz</option>
+                                    <option value='16384'>16384 hz</option>
                                 </Form.Control>
                             </Form.Group>
                         </Col>
                         <Col>
-                            <Form.Group controlId="signalDuration">
+                            <Form.Group controlId='signalDuration'>
                                 <Form.Label>Signal duration</Form.Label>
                                 <Form.Control
-                                    name="signalDuration"
-                                    as="select"
+                                    name='signalDuration'
+                                    as='select'
                                     custom
                                     {...formik.getFieldProps('signalDuration')}>
-                                    <option value="4">4 seconds</option>
-                                    <option value="8">8 seconds</option>
-                                    <option value="16">16 seconds</option>
-                                    <option value="24">24 seconds</option>
-                                    <option value="32">32 seconds</option>
-                                    <option value="64">64 seconds</option>
-                                    <option value="128">128 seconds</option>
+                                    <option value='4'>4 seconds</option>
+                                    <option value='8'>8 seconds</option>
+                                    <option value='16'>16 seconds</option>
+                                    <option value='24'>24 seconds</option>
+                                    <option value='32'>32 seconds</option>
+                                    <option value='64'>64 seconds</option>
+                                    <option value='128'>128 seconds</option>
                                 </Form.Control>
                             </Form.Group>
                         </Col>
@@ -100,10 +100,10 @@ const DataForm = ({formik, handlePageChange, data}) => {
                 </FormCard>
             </Col>
         </Row>
-        <CardDeck className="mb-3">
-            <DetectorCard channelOptions={hanford} title="Hanford" image={hanfordImg} formik={formik}/>
-            <DetectorCard channelOptions={livingston} title="Livingston" image={livingstonImg} formik={formik}/>
-            <DetectorCard channelOptions={virgo} title="Virgo" image={virgoImg} formik={formik}/>
+        <CardDeck className='mb-3'>
+            <DetectorCard channelOptions={hanford} title='Hanford' image={hanfordImg} formik={formik}/>
+            <DetectorCard channelOptions={livingston} title='Livingston' image={livingstonImg} formik={formik}/>
+            <DetectorCard channelOptions={virgo} title='Virgo' image={virgoImg} formik={formik}/>
         </CardDeck>
         <Row>
             <Col>
