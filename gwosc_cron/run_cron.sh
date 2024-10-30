6ccaeca0a0a8c916ee59b7cf91c85e8d10004732
nemovc <nemovc@gmail.com>
2024-10-30 13:18:18 +1100

    Remove $(pwd) from run script

--- gwosc_cron/run_cron.sh
+++ gwosc_cron/run_cron.sh
@@ -2,4 +2,5 @@
 # Assuming that you've tagged the built docker image with gwosc_ingest
 # And created a sqlite.db file
 # And created a .env file
-docker run --env-file .env --mount type=bind,src="$(pwd)"/sqlite.db,target=/sqlite.db gwosc_ingest
+source .env
+docker run --env-file .env --mount type=bind,src="$HOST_DB_PATH",target="$DB_PATH" gwosc_ingest
