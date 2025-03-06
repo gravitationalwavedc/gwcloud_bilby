7b4f1f5399172d20a98d06536ff0825c841a26d9
nemovc <nemovc@gmail.com>
2025-03-06 06:11:45 +0000

    Update test case
    Since we are no longer authenticating with JWTs we don't need to have
    our test runner support JWTs

--- src/bilbyui/tests/testcases.py
+++ src/bilbyui/tests/testcases.py
@@ -1,28 +1,13 @@
-from django.test import testcases, override_settings
-from graphql_jwt.testcases import JSONWebTokenClient
+from django.test import override_settings
 from gw_bilby.schema import schema
-from graphql_jwt.shortcuts import get_token
-from graphql_jwt.settings import jwt_settings
 
-
-class BilbyJSONWebTokenClient(JSONWebTokenClient):
-    """Bilby test client with a custom authentication method."""
-
-    def authenticate(self, user, is_ligo=False):
-        """Payload for authentication in bilby requires a special userID parameter."""
-
-        if user:
-            self._credentials = {
-                jwt_settings.JWT_AUTH_HEADER_NAME: "{0} {1}".format(
-                    jwt_settings.JWT_AUTH_HEADER_PREFIX, get_token(user, userId=user.id, isLigo=is_ligo)
-                ),
-            }
-        else:
-            self._credentials = {}
+from graphene_django.utils.testing import GraphQLTestCase
+import datetime
+from adacs_sso_plugin.test_client import ADACSSSOGraphqlSessionClient
 
 
 @override_settings(IGNORE_ELASTIC_SEARCH=True)
-class BilbyTestCase(testcases.TestCase):
+class BilbyTestCase(GraphQLTestCase):
     """
     Bilby test classes should inherit from this class.
 
@@ -44,18 +29,23 @@ class BilbyTestCase(testcases.TestCase):
 
     GRAPHQL_SCHEMA = schema
     GRAPHQL_URL = "/graphql"
-    client_class = BilbyJSONWebTokenClient
+    client_class = ADACSSSOGraphqlSessionClient
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # We always want to see the full diff when an error occurs.
         self.maxDiff = None
 
-    def assertResponseHasNoErrors(self, resp, msg=None):
-        """Semi-borrowed from graphene_django.utils.testing
-        They also check status_code, which we don't have access to"""
-        self.assertIsNone(resp.errors, msg or resp)
-
-    def assertResponseHasErrors(self, resp, msg=None):
-        """Borrowed from graphene_django.utils.testing"""
-        self.assertIsNotNone(resp.errors, msg or resp)
+    def default_authenticate(self):
+        self.client.authenticate(
+            {
+                "is_authenticated": True,
+                "id": 1,
+                "name": "buffy summers",
+                "primary_email": "slayer@gmail.com",
+                "emails": ["slayer@gmail.com"],
+                "authentication_method": "password",
+                "authenticated_at": datetime.datetime.now(tz=datetime.UTC).timestamp(),
+                "fetched_at": datetime.datetime.now(tz=datetime.UTC).timestamp(),
+            }
+        )
