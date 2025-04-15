a784ffeac10b25bb96b71318fcf2135ed1f18451
nemovc <nemovc@gmail.com>
2025-04-15 07:29:08 +0000

    We don't need static files

--- runserver.sh
+++ runserver.sh
@@ -3,5 +3,4 @@ while ! nc -z db 3306; do
   sleep 1
 done
 /venv/bin/python /src/production-manage.py migrate
-/venv/bin/python /src/production-manage.py collectstatic --noinput
 /venv/bin/gunicorn gw_bilby.wsgi --bind 0.0.0.0:8000 --workers 8 --timeout 600
