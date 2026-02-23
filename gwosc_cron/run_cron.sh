8049cd5149dc4b4a95d119aaaa24b4e2a2a283a7
Lewis Lakerink <llakerink@swin.edu.au>
2026-02-23 13:03:21 +1100

    Fix GWOSC ingest log mounting path
    - Update the log file mount target to the correct internal directory.
    - Ensure log persistence by matching the application logging path inside the container.
    
    Approved-by: Asher <jasherleslie@gmail.com>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwcloud_bilby/-/merge_requests/158

--- gwosc_cron/run_cron.sh
+++ gwosc_cron/run_cron.sh
@@ -5,4 +5,4 @@
 source .env
 # Ensure log file exists and is writable by the container (non-root) when bind-mounted
 touch ./gwosc_ingest.log && chmod a+w ./gwosc_ingest.log
-sudo docker run --env-file .env --network=host --mount type=bind,src="$HOST_DB_PATH",target="$DB_PATH" --mount type=bind,src="./gwosc_ingest.log",target="/gwosc_ingest.log" gwosc_ingest
+sudo docker run --env-file .env --network=host --mount type=bind,src="$HOST_DB_PATH",target="$DB_PATH" --mount type=bind,src="./gwosc_ingest.log",target="/app/gwosc_ingest.log" gwosc_ingest
