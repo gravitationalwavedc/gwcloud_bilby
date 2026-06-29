e56ac2f2619bf277dbb39a92a007cba85aed97d1
Lewis Lakerink <llakerink@swin.edu.au>
2026-06-29 16:03:16 +1000

    chore: simplify manage.py to standard Django template

--- src/manage.py
+++ src/manage.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-
 import os
 import sys
 
 
 def main():
-    # Mock gwosc event resolution during testing to avoid network timeouts
-    if "test" in sys.argv:
-
-        class MockDatasets:
-            @staticmethod
-            def event_gps(event):
-                event_map = {
-                    "GW150914": 1126259462.0,
-                    "GW151226": 1135136350.0,
-                }
-                if event in event_map:
-                    return event_map[event]
-                raise ValueError(f"Unknown event {event}")
-
-        sys.modules["gwosc.datasets"] = MockDatasets
-
+    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gw_bilby.dev")
     try:
         from django.core.management import execute_from_command_line
