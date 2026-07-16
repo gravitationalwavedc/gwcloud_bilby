375be9391fa826df0e050ebcb1801c7084d4deba
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-16 13:28:34 +1000

    chore(gardener): add noqa SLF001 to intentional _meta name max_length access in job_validation.py

--- src/bilbyui/utils/job_validation.py
+++ src/bilbyui/utils/job_validation.py
@@ -8,7 +8,7 @@ def validate_job_name(name):
     if len(name) < 5:
         raise Exception("Job name must be at least 5 characters long.")
 
-    max_len = BilbyJob._meta.get_field("name").max_length
+    max_len = BilbyJob._meta.get_field("name").max_length  # noqa: SLF001
     # this one is enforced by the database field's max_length
     if len(name) > max_len:
         raise Exception(f"Job name must be less than {max_len} characters long.")
