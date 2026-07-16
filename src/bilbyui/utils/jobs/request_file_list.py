4beeddce71db4d7a4a7e32d11f2af7e1125a6a75
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-17 00:31:05 +1000

    chore(gardener): replace os.path/os.listdir with pathlib.Path in request_file_list.py (PTH)

--- src/bilbyui/utils/jobs/request_file_list.py
+++ src/bilbyui/utils/jobs/request_file_list.py
@@ -1,5 +1,6 @@
 import contextlib
 import os
+from pathlib import Path
 
 from django.conf import settings
 
@@ -22,16 +23,17 @@ def request_file_list(job, path, recursive, user_id=None):
         job_dir = job.get_upload_directory()
 
         # Get the absolute path to the requested path
-        dir_path = os.path.abspath(os.path.join(job_dir, path))
+        dir_path = str(Path(job_dir, path).resolve())
 
         # Verify that:-
         # * this file really sits under the working directory
         # * the path exists
         # * the path is a directory
+        dir_path_obj = Path(dir_path)
         if (
             not (dir_path == job_dir or dir_path.startswith(job_dir + os.sep))
-            or not os.path.exists(dir_path)
-            or not os.path.isdir(dir_path)
+            or not dir_path_obj.exists()
+            or not dir_path_obj.is_dir()
         ):
             return False, "Files do not exist"
 
@@ -43,43 +45,43 @@ def request_file_list(job, path, recursive, user_id=None):
                 # Iterate over the directories
                 for item in dirnames:
                     # Construct the real path to this directory
-                    real_file_name = os.path.join(root, item)
+                    real_file_name = Path(root, item)
                     # Add the file entry
                     file_list.append(
                         {
                             # Remove the leading working directory
-                            "path": real_file_name[len(job_dir) :],
+                            "path": str(real_file_name)[len(job_dir) :],
                             "isDir": True,
-                            "fileSize": os.path.getsize(real_file_name),
+                            "fileSize": real_file_name.stat().st_size,
                         }
                     )
 
                 for item in filenames:
                     # Construct the real path to this file
-                    real_file_name = os.path.join(root, item)
+                    real_file_name = Path(root, item)
                     # Add the file entry
                     with contextlib.suppress(FileNotFoundError):
                         # Happens when trying to stat a symlink
                         file_list.append(
                             {
                                 # Remove the leading working directory
-                                "path": real_file_name[len(job_dir) :],
+                                "path": str(real_file_name)[len(job_dir) :],
                                 "isDir": False,
-                                "fileSize": os.path.getsize(real_file_name),
+                                "fileSize": real_file_name.stat().st_size,
                             }
                         )
         else:
             # Not a recursive search
-            for item in os.listdir(dir_path):
+            for item in dir_path_obj.iterdir():
                 # Construct the real path to this file/directory
-                real_file_name = os.path.join(dir_path, item)
+                real_file_name = item
                 # Add the file entry
                 file_list.append(
                     {
                         # Remove the leading working directory
-                        "path": real_file_name[len(job_dir) :],
-                        "isDir": os.path.isdir(real_file_name),
-                        "fileSize": os.path.getsize(real_file_name),
+                        "path": str(real_file_name)[len(job_dir) :],
+                        "isDir": real_file_name.is_dir(),
+                        "fileSize": real_file_name.stat().st_size,
                     }
                 )
 
