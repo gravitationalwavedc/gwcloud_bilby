e7a6f9fd4172e48f5f30e54377cceef9d23fcc48
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-21 16:26:33 +1000

    fix: remove duplicate User import

--- src/bilbyui/tests/test_parameter_output.py
+++ src/bilbyui/tests/test_parameter_output.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from django.contrib.auth import get_user_model
 
 from bilbyui.models import BilbyJob
-from bilbyui.tests.testcases import BilbyTestCase, User
+from bilbyui.tests.testcases import BilbyTestCase
 from bilbyui.utils.gen_parameter_output import generate_parameter_output, to_dec
 
 User = get_user_model()
