1ee9c6e2b807c9e5c40adf3729c96721145572f4
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-23 14:26:06 +1000

    chore: simplify test script commands and fix poetry run usage
    Replace `poetry run bash` with plain `bash` in README and AGENTS.md. Shell scripts now call `poetry run python` internally instead of bare `python`.
    
    Approved-by: Lewis Lakerink <llakerink@swin.edu.au>
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwcloud_bilby/-/merge_requests/416

--- src/run_coverage.sh
+++ src/run_coverage.sh
@@ -3,7 +3,7 @@ set -e
 # Run Django tests with coverage; always use coverage's parallel mode so it
 # works whether or not Django's --parallel is used. All arguments are passed
 # straight through to manage.py test.
-# Use from src/ with: poetry run bash run_coverage.sh [args]
+# Use from src/ with: bash run_coverage.sh [args]
 # Examples:
 #   ./run_coverage.sh
 #   ./run_coverage.sh --failfast bilbyui.tests.test_models
@@ -13,7 +13,7 @@ set -e
 # isn't using --parallel you'll just get a single data file; if it is, each
 # worker process will write its own, and combine will merge them.
 export DJANGO_SETTINGS_MODULE=gw_bilby.test
-python -m coverage erase
-python -m coverage run --parallel-mode manage.py test "$@"
-python -m coverage combine
-python -m coverage report
+poetry run python -m coverage erase
+poetry run python -m coverage run --parallel-mode manage.py test "$@"
+poetry run python -m coverage combine
+poetry run python -m coverage report
--- src/run_tests.sh
+++ src/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 # Run Django tests without coverage. All arguments are passed to manage.py test.
-# Use from src/ with: poetry run bash run_tests.sh [args]
+# Use from src/ with: bash run_tests.sh [args]
 # Examples:
 #   ./run_tests.sh
 #   ./run_tests.sh --failfast
 #   ./run_tests.sh bilbyui.tests.test_models
 #   ./run_tests.sh --parallel 4
 export DJANGO_SETTINGS_MODULE=gw_bilby.test
-python manage.py test "$@"
+poetry run python manage.py test "$@"
