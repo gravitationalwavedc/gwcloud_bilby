e3c5f3ad17ee2688948bc7c51d2c59d99b022424
Lewis Lakerink <llakerink@swin.edu.au>
2026-07-24 13:45:38 +1000

    chore(gardener): Bundle-level cancel function delegates to scheduler via get_scheduler()

--- src/bundle/core/cancel.py
+++ src/bundle/core/cancel.py
@@ -1,2 +1,17 @@
+from core.misc import get_scheduler
+
+
 def cancel(details, job_data):
-    pass
+    """
+    Cancel a running job
+
+    :param details: The job details object from the client
+    :param job_data: The internal job db record for the job to cancel
+    :return: True if the job was cancelled otherwise False
+    """
+    sched = get_scheduler()
+
+    if "submit_id" in job_data:
+        return sched.cancel(job_data["submit_id"], details)
+
+    return False
