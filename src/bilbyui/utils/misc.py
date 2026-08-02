ad7bc32321ae37439a91bc101518eac9fe76c585
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-02 17:57:11 +1000

    chore(gardener): Add bool return type annotation to is_ligo_user function (ANN201)

--- src/bilbyui/utils/misc.py
+++ src/bilbyui/utils/misc.py
@@ -19,5 +19,5 @@ def check_request_leak_decorator(fn):
     return wrapper
 
 
-def is_ligo_user(user):
+def is_ligo_user(user) -> bool:
     return not user.is_anonymous and AUTHENTICATION_METHODS["LIGO_SHIBBOLETH"] in user.authentication_methods
