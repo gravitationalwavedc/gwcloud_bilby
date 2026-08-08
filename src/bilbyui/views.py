720304bd68b65d8b4e4ef581e4779981524e648a
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-08 12:04:10 +1000

    chore(gardener): Convert f-string logger call to %-style lazy formatting in parse_supporting_files
    ## Summary
    - Convert an f-string logger call to %-style lazy formatting in parse_supporting_files (ruff G004).
    
    ## Scope
    - Single concern: G004 f-string in logger call
    - Files: src/bilbyui/views.py
    
    ## Test plan
    - [ ] poetry run ruff check src/bilbyui/views.py --select G004
    - [ ] poetry run bash test_coverage.sh
    
    ## Reviewer notes
    - No migrations, no vendored assets, no unrelated changes
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwcloud_bilby/-/merge_requests/655

--- src/bilbyui/views.py
+++ src/bilbyui/views.py
@@ -409,7 +409,7 @@ def parse_supporting_files(parser, args, prior_file, gps_file, timeslide_file, i
             setattr(args, config_name, None)
 
         else:
-            logger.error(f"Got unknown supporting file type for {config_name}: {config}")
+            logger.error("Got unknown supporting file type for %s: %s", config_name, config)
 
     return supporting_files
 
