6d7fa3caf36293b44965c557ec31b98fcdc7577f
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-09 17:17:04 +1000

    chore(gardener): guard ES update on user lookup failure
    ## Summary
    - BilbyJob.elastic_search_update now checks the success flag from request_lookup_users and returns early on lookup failure instead of indexing users[0] unconditionally (fixes TypeError 500 when auth service is down); added regression test
    
    ## Scope
    - Single concern: guard ES update on user lookup failure
    - Files: src/bilbyui/models.py, src/bilbyui/tests/test_elastic_search.py
    
    ## Test plan
    - [ ] poetry run bash test_coverage.sh
    
    ## Reviewer notes
    - No migrations, no vendored assets, no unrelated changes
    
    Merged by: Lewis Lakerink <llakerink@swin.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/gwcloud_bilby/-/merge_requests/772

--- src/bilbyui/models.py
+++ src/bilbyui/models.py
@@ -380,7 +380,9 @@ class BilbyJob(models.Model):
         )
 
         # Get the user details for this job
-        _, users = request_lookup_users([self.user.id])
+        success, users = request_lookup_users([self.user.id])
+        if not success:
+            return
         user = users[0]
 
         # Generate the document for insertion or update in elastic search
