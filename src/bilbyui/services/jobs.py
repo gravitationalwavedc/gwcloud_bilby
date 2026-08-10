c99c4e496e45cbbb2626bc074326a426f4a9e10b
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-10 15:02:16 +1000

    chore(gardener): Filter out non-numeric ES _id in list_public_jobs

--- src/bilbyui/services/jobs.py
+++ src/bilbyui/services/jobs.py
@@ -136,6 +136,8 @@ def list_public_jobs(user, *, search="", time_range="all", page=1, page_size=20,
     records = results["hits"]["hits"]
     has_next = len(records) > page_size
 
+    records = [record for record in records if isinstance(record["_id"], int) or str(record["_id"]).isdigit()]
+
     qs_before = (
         BilbyJob.objects.filter(id__in=[record["_id"] for record in records])
         .select_related("event_id")
