964168070dadbdad4a6539d04459c4d99380b835
Lewis Lakerink <llakerink@swin.edu.au>
2026-08-12 07:35:23 +1000

    chore(gardener): guard null user/job sections in public job ES records

--- src/bilbyui/tests/test_build_public_job_rows.py
+++ src/bilbyui/tests/test_build_public_job_rows.py
@@ -70,6 +70,16 @@ class TestBuildPublicJobRows(BilbyTestCase):
         self.assertEqual(rows[0]["name"], "Test job")
         self.assertEqual(rows[0]["user"], "buffy summers")
 
+    def test_record_with_null_user_and_job_sections(self):
+        job = self._create_job()
+        record = {"_id": str(job.id), "_source": {"user": None, "job": None}}
+        rows = _build_public_job_rows(_result([record], {job.id: job}))
+
+        self.assertEqual(len(rows), 1)
+        self.assertEqual(rows[0]["user"], "")
+        self.assertEqual(rows[0]["name"], "")
+        self.assertEqual(rows[0]["description"], "")
+
     def test_normal_job_statuses(self):
         with_controller = self._create_job(job_type=BilbyJobType.NORMAL)
         without_controller = self._create_job(
