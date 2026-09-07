943f63225a27ce616346811cbdbe43a1efdc984f
Lewis Lakerink <llakerink@swin.edu.au>
2026-09-08 07:29:24 +1000

    chore(gardener): move on_bilby_job_label_add_rem tests into test_models.py

--- src/bilbyui/tests/test_models.py
+++ src/bilbyui/tests/test_models.py
@@ -1,6 +1,7 @@
 import json
 import uuid
 from pathlib import Path
+from unittest import mock
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -14,6 +15,7 @@ from bilbyui.models import (
     FileDownloadToken,
     Label,
     SupportingFile,
+    on_bilby_job_label_add_rem,
 )
 from bilbyui.services.jobs import update_job
 from bilbyui.tests.test_utils import create_test_ini_string
@@ -252,6 +254,40 @@ class TestEventIDUpdate(BilbyTestCase):
         self.event.refresh_from_db()
         self.assertEqual(self.event.gps_time, 1126259462.391)
         self.assertEqual(self.event.event_id, "GW123456_123456")
+
+
+class TestOnBilbyJobLabelAddRem(BilbyTestCase):
+    def setUp(self):
+        self.instance = mock.Mock()
+
+    def test_post_add_calls_elastic_search_update(self):
+        on_bilby_job_label_add_rem(
+            sender=None,
+            instance=self.instance,
+            action="post_add",
+            pk_set={1},
+        )
+        self.instance.elastic_search_update.assert_called_once_with()
+
+    def test_post_remove_calls_elastic_search_update(self):
+        on_bilby_job_label_add_rem(
+            sender=None,
+            instance=self.instance,
+            action="post_remove",
+            pk_set={1},
+        )
+        self.instance.elastic_search_update.assert_called_once_with()
+
+    def test_other_actions_are_no_op(self):
+        for action in ["pre_add", "pre_remove", "post_clear", "pre_clear"]:
+            with self.subTest(action=action):
+                on_bilby_job_label_add_rem(
+                    sender=None,
+                    instance=self.instance,
+                    action=action,
+                    pk_set=set(),
+                )
+        self.instance.elastic_search_update.assert_not_called()
 
 
 class TestFileDownloadToken(BilbyTestCase):
