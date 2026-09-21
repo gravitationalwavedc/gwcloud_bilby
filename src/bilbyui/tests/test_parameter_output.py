7cd2524664984d667c8ee7d1a3acdaa37c1dbca9
Lewis Lakerink <llakerink@swin.edu.au>
2026-09-21 17:07:19 +1000

    chore(gardener): fix ruff-format in test_parameter_output

--- src/bilbyui/tests/test_parameter_output.py
+++ src/bilbyui/tests/test_parameter_output.py
@@ -71,7 +71,9 @@ class TestJobSubmission(BilbyTestCase):
                         "virgoMinimumFrequency": str(to_dec(float(rand_float(10, 900)))),
                         "virgoMaximumFrequency": str(to_dec(float(rand_float(1000, 20000)))),
                         "duration": str(to_dec(float(random.choice(["4", "8", "16", "32", "64", "128"])))),
-                        "samplingFrequency": str(to_dec(float(random.choice(["512", "1024", "2048", "4096", "8192", "16384"])))),
+                        "samplingFrequency": str(
+                            to_dec(float(random.choice(["512", "1024", "2048", "4096", "8192", "16384"])))
+                        ),
                     },
                     "prior": {"priorDefault": random.choice(["4s", "8s", "16s", "32s", "64s", "128s"])},
                     "sampler": {
